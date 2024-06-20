0746c7d6225d6d5ea8d98563d83eab205c824895
jessicasantospereira <jessydfj@gmail.com>
2024-06-20 14:54:58 -0300

    feat: criando primeiro componente

---
+++ src/components/Aside/index.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import styles from "./aside.module.css";
+import logo from "./logo.png";
+
+export const Aside = () => {
+  return (
+    <aside>
+      <Image
+        className={styles.aside}
+        src={logo}
+        alt="Logo da Code Connect"
+        width={100}
+        height={50}
+      />
+    </aside>
+  );
+};
