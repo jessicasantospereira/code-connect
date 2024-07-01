cc25d5d13a92de38deb8f856309989bec9cd1f72
jessicasantospereira <jessydfj@gmail.com>
2024-07-01 14:05:24 -0300

    feat: projeto final

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import { CardPost } from "@/components/CardPost";
 import style from "./page.module.css"
 import logger from "@/logger";
+import Link from "next/link";
 
 async function getAllPosts(page) {
   const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`)
@@ -12,11 +13,16 @@ async function getAllPosts(page) {
   return response.json()
 }
 
-export default async function Home() {
-  const { data: posts } = await getAllPosts(1);
+export default async function Home({ searchParams }) {
+  const currentPage = searchParams?.page || 1;
+  const { data: posts, prev, next } = await getAllPosts(currentPage);
   return (
     <main className={style.grid}>
       {posts.map(post => <CardPost key={post.id} post={post} />)}
+      <div className={style.links}>
+        {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
+        {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
+      </div>
     </main>
   );
 }
--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -1,27 +1,30 @@
 import Image from "next/image";
 import { Avatar } from "../Avatar";
 import style from "./cardpost.module.css";
+import Link from "next/link";
 
-export const CardPost = ({ post }) => {
+export const CardPost = ({ post, highlight }) => {
   return (
-    <article>
-      <header className={style.header}>
-        <figure>
-          <Image
-            src={post.cover}
-            width={438}
-            height={133}
-            alt={`Capa do post de titulo: ${post.title}`}
-          />
-        </figure>
-      </header>
-      <section className={style.body}>
-        <h2>{post.title}</h2>
-        <p>{post.body}</p>
-      </section>
-      <footer className={style.footer}>
-        <Avatar imageSrc={post.author.avatar} name={post.author.username} />
-      </footer>
-    </article>
+    <Link href={`/post/${post.slug}`} className={style.link}>
+      <article className={style.card} style={{ width: highlight ? 993 : 486 }}>
+        <header className={style.header}>
+          <figure style={{ height: highlight ? 300 : 133 }}>
+            <Image
+              src={post.cover}
+              width={438}
+              height={133}
+              alt={`Capa do post de titulo: ${post.title}`}
+            />
+          </figure>
+        </header>
+        <section className={style.body}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </section>
+        <footer className={style.footer}>
+          <Avatar imageSrc={post.author.avatar} name={post.author.username} />
+        </footer>
+      </article>
+    </Link>
   );
 };
